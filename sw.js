2b5d2eb21ad5a1fe486bc380af9529eb7505bd0c
SergioMendes52 <a27335@aecister.pt>
2026-05-15 13:02:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,53 @@
-// Este evento corre quando a App é "instalada" no telemóvel
-self.addEventListener('install', (e) => {
- console.log('Service Worker: Instalado');
+const cacheName = 'O Botão v1';
+
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './README.txt',
+  './assets/icon.png',
+  './assets/css/style.css', 
+  './assets/js/script.js',  
+  
+  // Imagens da pasta assets/images
+  './assets/images/icon-192.png',
+  './assets/images/icon-512.jpg',
+  './assets/images/pap-preview.jpg',
+  './assets/images/perfil.jpg',
+ 
+
+  
+  // Páginas
+
+];
+
+// Instalação: Guarda os ficheiros na Cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('App O Botão: A guardar ficheiros offline...');
+      return cache.addAll(assets);
+    })
+  );
 });
-// Este evento permite que a App funcione mesmo com picos de falta de rede
-self.addEventListener('fetch', (e) => {
-    // Para este projeto base, apenas deixamos o tráfego passar
- e.respondWith(fetch(e.request));
+
+// Ativação: Limpa lixo de versões antigas
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch: Serve os ficheiros da Cache se estiver offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
